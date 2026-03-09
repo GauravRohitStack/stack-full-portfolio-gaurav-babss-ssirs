ec1324fb15b82c3578e368610a3507227c4776d9
Gaurav Rohit Singh <gauravstackfull@gmail.com>
2026-03-09 23:08:29 +0530

    Final update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,73 @@
 
 
+// import "../styles/footer.css";
+
+// const Footer = () => {
+//   return (
+//     <footer className="footer">
+
+//       <div className="footer-overlay">
+//         <p>©2026 Rohit Singh Gaurav</p>
+
+//         <div className="footer-links">
+//           <a href="" target="_blank" rel="noreferrer">
+//             GitHub
+//           </a>
+//           {" | "}
+//           <a href="" target="_blank" rel="noreferrer">
+//             LinkedIn
+//           </a>
+//         </div>
+//       </div>
+
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
+
+
+
+
+
 import "../styles/footer.css";
+import banner from "../assets/footer-banner.jpg";   // your banner image
 
 const Footer = () => {
   return (
-    <footer className="footer">
-
+    <footer
+      className="footer"
+      style={{ backgroundImage: `url(${banner})` }}
+    >
       <div className="footer-overlay">
-        <p>©2026 Rohit Singh Gaurav</p>
+
+        <p className="footer-text">© 2026 Rohit Singh Gaurav</p>
 
         <div className="footer-links">
-          <a href="https://github.com/GauravRohitStack" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/GauravRohitStack"
+            target="_blank"
+            rel="noreferrer"
+          >
             GitHub
           </a>
-          {" | "}
-          <a href="https://linkedin.com/in/rohit-s-gaurav-108331192" target="_blank" rel="noreferrer">
+
+           {" | "}
+
+          <a
+            href="linkedin.com/in/rohit-s-gaurav-108331192"
+            target="_blank"
+            rel="noreferrer"
+          >
             LinkedIn
           </a>
         </div>
-      </div>
 
+      </div>
     </footer>
   );
 };
 
 export default Footer;
-
-
-
-
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,11 +9,15 @@ function About() {
         I enjoy solving complex backend problems and creating
         clean and responsive user interfaces.
       </p>
-
       <p style={{ marginTop: "10px" }}>
         I have built multiple projects including Email_Schedular, User_Management_System, Mini_CRM_Business_Systems,
         Payment_APP_Systems, & Multiple Website Develop, deploy & host and automation tools.
       </p>
+      <p>I have worked in agile team and contributes to backend development, frontend development, API Integration and database management system. </p>
+      <p>I write and maintain Java code that follows SOLID principles and design patterns such as Singleton, Factory, and Strategy.</p>
+      <p>I work with React.js to develop dynamic and reusable User interfaces and integrate them with RESTful APIs, and ensure responsive designs that improve user experience across browsers.</p>
+      <p>I use Maven and Gradle for dependency management and build automation in all of my projects.</p>
+      <p>Additionally, I support production releases, monitor logs using tools like Splunk, and quickly resolve bugs or performance issues.</p>
     </section>
   );
 }
